672bde96995c3ea93b65cc0efa06d1f1f801a3be
Mariagoretti Kimani <mgmkimani@gmail.com>
2022-12-07 17:42:01 +0300

    palidrome

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const lenght = word.length;// checks the length of the word
+  for (let j=0; j<lenght/2; j++){// divides the words into 2 half
+    if (word[j]===word[lenght-1-j]){   // check if the first and last characters are same
+      return true;
+    }else {
+      return false;
+    }
+  }
+
 }
 
-/* 
+/*
   Add your pseudocode here
+  1. function ispalindrome accepts an argument as string
+  2.get the length of the string
+  3. divide the string into two half
+  4check if the first and the last characters are the same
+  5. returns true if the characters are the same
+  6.returns false if the characters are not the same
 */
 
 /*
-  Add written explanation of your solution here
+  the solution checks if the word string is palindrome which means it can be reversed and be
+  similar as the word is written originally. if the word is a palindrome it returns true otherwise it returns false.
 */
 
 // You can run `node index.js` to view these console logs
